27153db1a7f113f3b80d2f7fc58312e65d5dcdf7
bodo156 <borisstrbak@gmail.com>
2019-05-08 13:13:35 +0200

    Odstranenie nepotrebnzch suborov

--- proj1/ivs_proj1_ver2/ivs_project_1_2018/assignment/build/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/boris/Desktop/ivs_proj1_ver2/ivs_project_1_2018/assignment/build/googletest-master/googlemock
-# Build directory: /home/boris/Desktop/ivs_proj1_ver2/ivs_project_1_2018/assignment/build/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
